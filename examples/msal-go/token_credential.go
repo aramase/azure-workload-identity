fcb4be662a723135bad4950c7ad7ee26e3f3cef7
Anish Ramasekar <anish.ramasekar@gmail.com>
2023-09-12 18:21:27 +0000

    demo app changes for krossota
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- examples/msal-go/token_credential.go
+++ examples/msal-go/token_credential.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
@@ -37,7 +42,10 @@ func newClientAssertionCredential(tenantID, clientID, authorityHost, file string
 		},
 	)
 
-	client, err := confidential.New(fmt.Sprintf("%s%s/oauth2/token", authorityHost, tenantID), clientID, cred)
+	authority := fmt.Sprintf("%s%s/oauth2/token", authorityHost, tenantID)
+	client, err := confidential.New(authority, clientID, cred,
+		confidential.WithInstanceDiscovery(false), confidential.WithHTTPClient(createCustomHTTPClient(tenantID)))
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to create confidential client: %w", err)
 	}
@@ -72,4 +80,73 @@ func (c *clientAssertionCredential) getAssertion(context.Context) (string, error
 		c.lastRead = now
 	}
 	return c.assertion, nil
+}
+
+func createCustomHTTPClient(tenantID string) *http.Client {
+	return &http.Client{
+		Transport: &customHTTPClient{tenantID: tenantID},
+	}
+}
+
+type customHTTPClient struct {
+	tenantID string
+}
+
+func (c *customHTTPClient) RoundTrip(req *http.Request) (*http.Response, error) {
+	if strings.Contains(req.URL.String(), ".well-known/openid-configuration") {
+		fmt.Println("requesting openid-configuration")
+		resp, err := http.DefaultTransport.RoundTrip(req)
+		if err != nil {
+			return nil, err
+		}
+
+		// read the response
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		// set the authorization_endpoint in the original response
+
+		type response struct {
+			Issuer                           string   `json:"issuer"`
+			JwksURI                          string   `json:"jwks_uri"`
+			AuthorizationEndpoint            string   `json:"authorization_endpoint"`
+			TokenEndpoint                    string   `json:"token_endpoint"`
+			ResponseTypesSupported           []string `json:"response_types_supported"`
+			SubjectTypesSupported            []string `json:"subject_types_supported"`
+			IDTokenSigningAlgValuesSupported []string `json:"id_token_signing_alg_values_supported"`
+		}
+
+		var r response
+		if err := json.Unmarshal(body, &r); err != nil {
+			return nil, err
+		}
+		// TODO(aramase): use the authority host env var
+		r.AuthorizationEndpoint = "https://krossota.monis.app/" + c.tenantID + "/oauth2/v2.0/token"
+		r.TokenEndpoint = "https://krossota.monis.app/" + c.tenantID + "/oauth2/v2.0/token"
+
+		body, err = json.Marshal(r)
+		if err != nil {
+			return nil, err
+		}
+
+		resp = &http.Response{
+			Status:        "200 OK",
+			StatusCode:    200,
+			Proto:         "HTTP/1.1",
+			ProtoMajor:    1,
+			ProtoMinor:    1,
+			Body:          nil,
+			ContentLength: int64(len(body)),
+			Request:       req,
+		}
+
+		// set the response body
+		resp.Body = io.NopCloser(bytes.NewReader(body))
+
+		return resp, nil
+	}
+
+	return http.DefaultTransport.RoundTrip(req)
 }
